bdcec97a57289b50c9803ce79281c66b9990630b
Rustam Parpiev <ichigo00712345@gmail.com>
2017-07-08 19:03:05 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp'),
+	sass = require('gulp-sass');
+	browserSync = require('browser-sync');
+
+
+gulp.task('styles', function () {
+	gulp.src('app/assets/sass/*.scss')
+	.pipe(sass())
+	.pipe(gulp.dest('app/dist/styles'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch('app/assets/sass/*.scss', ['styles']);
+	gulp.watch('app/dist/styles/*.css', browserSync.reload);
+	gulp.watch('app/*.html', browserSync.reload);
+});
+
+gulp.task('browserSync', function() {
+  browserSync({
+    server: {
+      baseDir: "./app"
+    },
+    port: 9000,
+    open: true,
+    notify: false
+  });
+});
+
+gulp.task('default', ['styles', 'watch', 'browserSync']);
